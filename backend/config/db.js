fc540acb50dd684b98924f25cdb66900e1c71ddd
Yashvi Smart <yashvipsmart@gmail.com>
2026-08-21 19:29:08 +0530

    Initial project strucutre

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+//connection to the database. TO use this connecion you can do const db = require("../config/db"); in your files
+
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+module.exports = pool;
