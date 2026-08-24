f3d679cf4e428ba7e5b8579ef0daf97ae108caab
Yashvi Smart <yashvipsmart@gmail.com>
2026-08-24 21:33:29 +0530

    Combined checkFields in registerUser, implemented checkEmail

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,17 +4,14 @@ const db = require("../config/db")
 //register a user
 //taking in name, password(will need to be hashed), email
 
-const checkFeilds = (userData) =>{
-        if(!userData.email || !userData.password ||!userData.name){
-            return res.status(400).json({
-                message: "Please fill all the required feilds"
-            })
-        }
-        if (!userData.email && !userData.password && !userData.name) {
-          return res.status(400).json({
-            message: "Please fill all the required feilds",
-          });
-        }
+
+
+//check if a user is already registered
+const checkEmail = async(userData)=>{
+    const checkEmailQuery = `SELECT * FROM Users WHERE email = ?`
+
+    const [result] = await db.query(checkEmailQuery, [userData.email])
+    return result
 }
 
 //follows async await approach
@@ -33,25 +30,50 @@ const registerUser = async(req, res) =>{
         password: req.body.password,
         name: req.body.name
     }
-    checkFeilds(userData)
-    //hash the password 
-    userData.hashed_password = await bcrypt.hash(userData.password,10) //bcrypt.hash is asynchrnous
-    //try and catch used with async await to execute createUser
-
-    /*If promise is resolved in createUser then the try block executes and success message is sent if the promise
-    is rejected createUser will not fully execute and the error message from catch block will be executed */
+      if (!userData.email || !userData.password || !userData.name) {
+        return res.status(400).json({
+          success: false,
+          message: "Please fill all the required fields",
+        });
+    };
+    //call checkEmail to check is user is already registered
     try{
-        const resultRegister = createUser(userData)
-        return res.status(200).json({
-            message: "User registered successfully"
-        })
+      const checkEmailResult = await checkEmail(userData);
+      if (checkEmailResult.length > 0) {
+        //user already exists
+        return res.status(409).json({
+          success: false,
+          message: "User is already registered",
+        });
+      }
+      //user does not exist
+      //hash the password
+      userData.hashed_password = await bcrypt.hash(userData.password, 10); //bcrypt.hash is asynchrnous
+      //try and catch used with async await to execute createUser
+
+      /*If promise is resolved in createUser then the try block executes and success message is sent if the promise
+    is rejected createUser will not fully execute and the error message from catch block will be executed */
+      try {
+        const resultRegister = await createUser(userData);
+        return res.status(201).json({
+          success: true,
+          message: "User registered successfully"
+        });
+      } catch (error) {
+        return res.status(500).json({
+          success: false,
+          message: "Error is registering user",
+          error: error.message,
+        });
+      }
     }
     catch(error){
         return res.status(500).json({
+            success: false,
             message: "Error is registering user",
             error: error.message
         })
-    }   
+    }
 }
 
 module.exports = {registerUser}
